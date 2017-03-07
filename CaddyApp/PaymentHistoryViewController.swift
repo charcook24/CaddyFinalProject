d6048e462ccc8bb9bd0821cb8fc6fe8710a965b3
charlescook <charlescook2017@depauw.edu>
2017-03-07 17:39:38 -0500

    added some storyboard views and updated UI

---
+++ CaddyApp/PaymentHistoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentHistoryViewController.swift
+//  CaddyApp
+//
+//  Created by Charles Cook on 3/7/17.
+//  Copyright © 2017 Charles Cook. All rights reserved.
+//
+
+import UIKit
+
+
+class PaymentHistoryViewController: UIViewController {
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
---
+++ CaddyApp/PerformanceHistoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PerformanceHistoryViewController.swift
+//  CaddyApp
+//
+//  Created by Charles Cook on 3/7/17.
+//  Copyright © 2017 Charles Cook. All rights reserved.
+//
+
+import UIKit
+
+class PerformanceHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+
+}
